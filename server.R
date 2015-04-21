02e56ed7972a20fcc6f75fe1c98911f6dc4aa495
crsExplorate <crsExplorate@users.noreply.github.com>
2015-04-21 22:39:40 +0200

    Update model

--- server.R
+++ server.R
@@ -7,56 +7,147 @@
 
 library(shiny)
 library(ggplot2)
+require(ROCR)
+
+findPerformance<-function(t,m){
+  findCorrespondence(t,rev(m@x.values[[1]]),rev(m@y.values[[1]]))
+}
+
+findCorrespondence<-function(t,x,y){
+  idx<-findInterval(t,x)
+  xlow<-x[idx]
+  xhigh<-x[idx+1]
+  if(!is.na(xhigh)){
+    idx=idx+round((t-xlow)/(xhigh-xlow))
+  }
+  y[idx]
+}
+  
+buildModel<-function(){
+  irisExtra<-iris
+  irisExtra$setosa<-irisExtra$Species=="setosa"
+  irisExtra$versicolor<-irisExtra$Species=="versicolor"
+  irisExtra$virginica<-irisExtra$Species=="virginica"
+  
+  
+  list(
+  "setosa"=list(
+    "data"=irisExtra$setosa,
+    "modA"=glm(setosa~Sepal.Length,irisExtra,family = "binomial"),
+    "modB"=glm(setosa~Sepal.Length+Sepal.Width,irisExtra,family = "binomial"),
+    "modC"=glm(setosa~Sepal.Length+Sepal.Width+Petal.Length+Petal.Width,irisExtra,family = "binomial")
+  ),
+  "versicolor"=list(
+    "data"=irisExtra$versicolor,
+    "modA"=glm(versicolor~Sepal.Length,irisExtra,family = "binomial"),
+    "modB"=glm(versicolor~Sepal.Length+Sepal.Width,irisExtra,family = "binomial"),
+    "modC"=glm(versicolor~Sepal.Length+Sepal.Width+Petal.Length+Petal.Width,irisExtra,family = "binomial")
+  ),
+  "virginica"=list(
+    "data"=irisExtra$virginica,
+    "modA"=glm(virginica~Sepal.Length,irisExtra,family = "binomial"),
+    "modB"=glm(virginica~Sepal.Length+Sepal.Width,irisExtra,family = "binomial"),
+    "modC"=glm(virginica~Sepal.Length+Sepal.Width+Petal.Length+Petal.Width,irisExtra,family = "binomial")
+  )
+ )
+}
+
+model<-buildModel()
+
 
 shinyServer(function(input, output) {
-
-  output$distPlot <- renderPlot({
-
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+  
+  buildPrediction<-function(){
+    prd<-predict(model[[input$predict]][[input$model]], type = "response")
+    predict<-prediction(prd,model[[input$predict]][["data"]])
+    predict
+  }
+  
+  
+  output$modelDOF<-renderText({ 
+    df.residual(model[[input$predict]][[input$model]])
   })
   
-  output$distPlot2 <- renderPlot({
+  output$modelAUC<-renderText({ 
+    as.numeric(performance(buildPrediction(),measure = "auc")@y.values)
+  })
+
+  output$modelAIC<-renderText({ 
+    AIC(model[[input$predict]][[input$model]])
+  })
+  
+  output$cutOffProb<-renderText({ 
+    formatC(input$prob*100.0, digits = 1, format = "f")
+  })
+  
+  
+  output$modelAccuracy<-renderText({ 
+    findPerformance(input$prob,performance(buildPrediction(),measure = "acc"))
+  })
+  
+  output$modelRecall<-renderText({ 
+    findPerformance(input$prob,performance(buildPrediction(),measure = "rec"))
+  })
+  
+  output$modelSensitivity<-renderText({ 
+    findPerformance(input$prob,performance(buildPrediction(),measure = "sens"))
+  })
+  
+  output$modelF1<-renderText({ 
+    findPerformance(input$prob,performance(buildPrediction(),measure = "f"))
+  })
+  
+  output$modelSpecificity<-renderText({ 
+    findPerformance(input$prob,performance(buildPrediction(),measure = "spec"))
+  })
+  
+  output$modelPrecision<-renderText({ 
+    findPerformance(input$prob,performance(buildPrediction(),measure = "prec"))
+  })
+  
+  
+  output$plotROCCurve <- renderPlot({
     
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    qplot(x, breaks = bins, col = 'darkgray', border = 'white')
+    # draw the performance Graph
+    plot(performance(buildPrediction(),"tpr","fpr"),print.cutoffs.at=c(input$prob),text.adj=c(-1.2, 1.2),colorize=TRUE)
     
   })
   
-  output$progressBox <- renderInfoBox({
-    infoBox(
-      "Progress", paste0(25 + input$count, "%"), icon = icon("list"),
-      color = "purple"
-    )
-  })
-  output$approvalBox <- renderValueBox({
-    valueBox(
-      value = formatC(input$bins/100.0, digits = 1, format = "f"),
-      subtitle = "Downloads per sec (last 5 min)",
-      icon = icon("thumbs-up", lib = "glyphicon"),
-      color = if (50 >= input$bins) "yellow" else "aqua"      
-    )
+  output$plotPrecissionRecall <- renderPlot({
+    
+    # draw the performance Graph
+    plot(performance(buildPrediction(),"prec","rec"),print.cutoffs.at=c(input$prob),text.adj=c(1.2, 1.2),colorize=TRUE)
+    
   })
   
-  output$packageTable <- renderTable({
-#     pkgData() %>%
-#       group_by(package) %>%
-#       tally() %>%
-#       arrange(desc(n), tolower(package)) %>%
-#       mutate(percentage = n / nrow(pkgData()) * 100) %>%
-#       select("Package name" = package, "% of downloads" = percentage) %>%
-#       as.data.frame() %>%
-#       head(15)
-    head(faithful,15)
+  output$plotSensitivitySpecifity <- renderPlot({
+    
+    # draw the performance Graph
+    plot(performance(buildPrediction(),"sens","spec"),print.cutoffs.at=c(input$prob),text.adj=c(1.2, 1.2),colorize=TRUE)
+    
+  })
+  
+  output$plotAccuracyCutoff <- renderPlot({
+    
+    # draw the performance Graph
+    plot(performance(buildPrediction(),"acc"))
+    abline(v=input$prob, col = "blue")
+  })
+  
+
+  output$plotModelAdjust <- renderPlot({
+    
+    # draw the model Graph
+    par(mfrow=c(2,2))
+    plot(model[[input$predict]][[input$model]])   
+  })
+  
+  
+  
+  output$coeffTables <- renderTable({
+    coefs<-as.data.frame(coefficients(model[[input$predict]][[input$model]]))
+    names(coefs)<-"Values"
+    coefs
   }, digits = 1)
 
 })
--- ui.R
+++ ui.R
@@ -5,46 +5,29 @@
 # http://shiny.rstudio.com
 #
 
-library(shiny)
-library(shinydashboard)
+require(shiny)
+require(shinydashboard)
 
-# shinyUI(fluidPage(
-# 
-#   # Application title
-#   titlePanel("Old Faithful Geyser Data"),
-# 
-#   # Sidebar with a slider input for number of bins
-#   sidebarLayout(
-#     sidebarPanel(
-#       sliderInput("bins",
-#                   "Number of bins:",
-#                   min = 1,
-#                   max = 50,
-#                   value = 30)
-#     ),
-# 
-#     # Show a plot of the generated distribution
-#     mainPanel(
-#       plotOutput("distPlot")
-#     )
-#   )
-# ))
-
-header<-dashboardHeader(title = "Confidence Model")
+header<-dashboardHeader(title = "Regresion Model")
 
 sidebar<-dashboardSidebar(
-  selectInput("variable", "Variable:",
-              c("Cylinders" = "cyl",
-                "Transmission" = "am",
-                "Gears" = "gear")),
+  selectInput("predict", "Detect:",
+              c("Setosa" = "setosa",
+                "Versicolor" = "versicolor",
+                "Virginica" = "virginica")),
+  selectInput("model", "Model:",
+              c("Model A" = "modA",
+                "Model B" = "modB",
+                "Model C" = "modC")),
   
-  sliderInput("bins", "Warn when rate exceeds",
-              min = 0, max = 100, value = 20, step = 0.1
+  sliderInput("prob", "Probability Cut",
+              min = 0, max = 1, value = 0.5, step = 0.01
   ),
   checkboxInput("outliers", "Show outliers", FALSE),
   sidebarMenu(
     menuItem("Main Board", tabName = "dashboard", icon = icon("dashboard")),
-    menuItem("Details", tabName = "widgets", icon = icon("th"))
+    menuItem("Model Details", tabName = "details", icon = icon("area-chart")),
+    menuItem("About", tabName = "about", icon = icon("th"))
   )
 )
 
@@ -53,38 +36,96 @@ body<-dashboardBody(
     tabItem(tabName = "dashboard",
             fluidRow(
               # A static infoBox
-              infoBox("New Orders", 10 * 2, icon = icon("credit-card")),
-              # Dynamic infoBoxes
-              infoBoxOutput("progressBox"),
-              infoBoxOutput("approvalBox")
+              infoBox("Accuracy", textOutput("modelAccuracy"), icon = icon("credit-card"),
+                      color="red"
+              ),
+              infoBox(
+                "Recall", textOutput("modelRecall"), icon = icon("list"),
+                color = "orange"
+              ),
+              infoBox(
+                "Sensitivity", textOutput("modelSensitivity"), icon = icon("thumbs-up", lib = "glyphicon"),
+                color = "purple"
+              )
+            ),
+            fluidRow(
+              # A static infoBox
+              infoBox("F1 Score", textOutput("modelF1"), icon = icon("credit-card"),
+                      color="red"
+              ),
+              infoBox(
+                "Specificity", textOutput("modelSpecificity"), icon = icon("list"),
+                color = "orange"
+              ),
+              infoBox(
+                "Precision", textOutput("modelPrecision"), icon = icon("thumbs-up", lib = "glyphicon"),
+                color = "purple"
+              )
             ),
             fluidRow(
               box(
-                width = 8, status = "info", solidHeader = TRUE,
-                title = "Popularity by package (last 5 min)",
-                plotOutput("distPlot", height = 450)
+                width = 5, status = "info", solidHeader = TRUE,
+                title = "ROC Curve",
+                plotOutput("plotROCCurve", height = 350)
               ),
               box(
-                width = 3, status = "info",
-                title = "Top packages (last 5 min)",
-                tableOutput("packageTable")
+                width = 5, status = "info", solidHeader = TRUE,
+                title = "Precission vs Recall",
+                plotOutput("plotPrecissionRecall", height = 350)
+              )
+              
+            ),
+            fluidRow(
+              box(
+                width = 5, status = "info", solidHeader = TRUE,
+                title = "Sensitivity vs Specifity",
+                plotOutput("plotSensitivitySpecifity", height = 350)
+              ),
+              box(
+                width = 5, status = "info", solidHeader = TRUE,
+                title = "Accuracy vs Cutoff",
+                plotOutput("plotAccuracyCutoff", height = 350)
               )
               
             )
     ),
     
     # Second tab content
-    tabItem(tabName = "widgets",
+    tabItem(tabName = "details",
             h2("Widgets tab content"),
+            fluidRow(
+              # A static infoBox
+              infoBox("Degrees of Freedom", textOutput("modelDOF"), icon = icon("credit-card"),
+                      color="red"
+              ),
+              infoBox(
+                "AUC", textOutput("modelAUC"), icon = icon("list"),
+                color = "orange"
+              ),
+              infoBox(
+                "AIC", textOutput("modelAIC"), icon = icon("thumbs-up", lib = "glyphicon"),
+                color = "purple"
+              )
+              
+            ),
             fluidRow(
               box(
                 width = 8, status = "info", solidHeader = TRUE,
-                title = "Grafico repetido",
-                plotOutput("distPlot2", height = 450)
+                title = "Model adjust details",
+                plotOutput("plotModelAdjust", height = 600)
+              ),
+              box(
+                width = 4, status = "info",
+                title = "Model Coefficeints",
+                tableOutput("coeffTables")
               )
               
             )
-    )
+    ),
+    tabItem(tabName = "about",
+            h2("About tab content"),
+            h3("http://google.es") 
+            )
   )
 )
 
