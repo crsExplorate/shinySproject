613ffc7c201a8273d2058906bad0ee70b6e41e21
crsExplorate <crsExplorate@users.noreply.github.com>
2015-04-21 02:48:36 +0200

    Simple Multipgraph example

--- server.R
+++ server.R
@@ -6,6 +6,7 @@
 #
 
 library(shiny)
+library(ggplot2)
 
 shinyServer(function(input, output) {
 
@@ -19,5 +20,43 @@ shinyServer(function(input, output) {
     hist(x, breaks = bins, col = 'darkgray', border = 'white')
 
   })
+  
+  output$distPlot2 <- renderPlot({
+    
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2]
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    qplot(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+  })
+  
+  output$progressBox <- renderInfoBox({
+    infoBox(
+      "Progress", paste0(25 + input$count, "%"), icon = icon("list"),
+      color = "purple"
+    )
+  })
+  output$approvalBox <- renderValueBox({
+    valueBox(
+      value = formatC(input$bins/100.0, digits = 1, format = "f"),
+      subtitle = "Downloads per sec (last 5 min)",
+      icon = icon("thumbs-up", lib = "glyphicon"),
+      color = if (50 >= input$bins) "yellow" else "aqua"      
+    )
+  })
+  
+  output$packageTable <- renderTable({
+#     pkgData() %>%
+#       group_by(package) %>%
+#       tally() %>%
+#       arrange(desc(n), tolower(package)) %>%
+#       mutate(percentage = n / nrow(pkgData()) * 100) %>%
+#       select("Package name" = package, "% of downloads" = percentage) %>%
+#       as.data.frame() %>%
+#       head(15)
+    head(faithful,15)
+  }, digits = 1)
 
 })
--- ui.R
+++ ui.R
@@ -30,18 +30,63 @@ library(shinydashboard)
 #   )
 # ))
 
-ui<-dashboardPage(
-  dashboardHeader(title = "Confidence Model"),
-  dashboardSidebar(),
-  dashboardBody(
-    fluidRow(
-      box(plotOutput("distPlot", height = 250)),
-      
-      box(
-        title = "Controls",
-        sliderInput("bins", "Number of bins:", min = 1, max = 50, value = 30)
-      )
-      
+header<-dashboardHeader(title = "Confidence Model")
+
+sidebar<-dashboardSidebar(
+  selectInput("variable", "Variable:",
+              c("Cylinders" = "cyl",
+                "Transmission" = "am",
+                "Gears" = "gear")),
+  
+  sliderInput("bins", "Warn when rate exceeds",
+              min = 0, max = 100, value = 20, step = 0.1
+  ),
+  checkboxInput("outliers", "Show outliers", FALSE),
+  sidebarMenu(
+    menuItem("Main Board", tabName = "dashboard", icon = icon("dashboard")),
+    menuItem("Details", tabName = "widgets", icon = icon("th"))
+  )
+)
+
+body<-dashboardBody(
+  tabItems(
+    tabItem(tabName = "dashboard",
+            fluidRow(
+              # A static infoBox
+              infoBox("New Orders", 10 * 2, icon = icon("credit-card")),
+              # Dynamic infoBoxes
+              infoBoxOutput("progressBox"),
+              infoBoxOutput("approvalBox")
+            ),
+            fluidRow(
+              box(
+                width = 8, status = "info", solidHeader = TRUE,
+                title = "Popularity by package (last 5 min)",
+                plotOutput("distPlot", height = 450)
+              ),
+              box(
+                width = 3, status = "info",
+                title = "Top packages (last 5 min)",
+                tableOutput("packageTable")
+              )
+              
+            )
+    ),
+    
+    # Second tab content
+    tabItem(tabName = "widgets",
+            h2("Widgets tab content"),
+            fluidRow(
+              box(
+                width = 8, status = "info", solidHeader = TRUE,
+                title = "Grafico repetido",
+                plotOutput("distPlot2", height = 450)
+              )
+              
+            )
     )
   )
 )
+
+
+ui<-dashboardPage(header,sidebar,body)
